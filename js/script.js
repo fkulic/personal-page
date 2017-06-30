b4bcdf18f4b1ac9c6e2c09117a5f982c049993f0
Filip Kulić <filip.kulic@outlook.com>
2017-06-30 15:15:11 +0200

    add personal-page v1.0

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+	let typedStrings = [""]
+
+	$("#quoteStrings").typed({
+		strings: [" android applications.", " web applications.", " web pages.", " arduino projects."],
+		startDelay: 100,
+		typeSpeed: 50,
+		backDelay: 1400,
+		loop: true
+	});
+});
